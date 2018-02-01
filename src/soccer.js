9626314b5f6b8b01691d87481bb3c8372d9348f4
James Robinson <jrobind@outlook.com>
2018-02-01 17:12:02 +0000

    add functionality to build table element and tableHead for league

--- src/soccer.js
+++ src/soccer.js
@@ -23,6 +23,7 @@ function updateCache(team) {
     cache.goalsScored = team.goalsScored;
 }
 
+
 function goalDifferenceCheck(team) {
     if (team.goalDiff > cache.goalDiff) {
         updateCache(team);
@@ -31,6 +32,7 @@ function goalDifferenceCheck(team) {
         goalsScoredCheck(team);
     }
 }
+
 
 function goalsScoredCheck(team) {
     if (team.goalsScored > cache.goalsScored) {
@@ -41,11 +43,13 @@ function goalsScoredCheck(team) {
     } 
 }
 
+
 function sortAlphabetically(team, cacheTeam) {
     if (team.name.toLowerCase() < cacheTeam.name.toLowerCase()) {
         updateCache(team);
     }
 }
+
 
 function cacheReset() {
     cache.team = null;
@@ -53,6 +57,7 @@ function cacheReset() {
     cache.goalDiff = 0;
     cache.goalsScored = 0;
 }
+
 
 function sortWithZeroPoints(zeroTeams) {
     
@@ -88,13 +93,78 @@ function sortWithZeroPoints(zeroTeams) {
 }
 
 
+function positionManually(team, position) {
+    // format position in relation to array indexing
+    var pos = position -1;
+    // add position property to the team 
+    team['position'] = pos;
+    
+    // add to tempArray but sort by manual position given
+    league.manualLeague[pos] = team;
+    
+    console.log(league.manualLeague);
+}
+
+function createTableHead(tableEl) {
+    // cell data for header
+    var headData = ['Team', 'GP', 'W', 'D', 'L', 'F', 'A', 'GD', 'Pts'];
+    // append table head to table
+    var tableHead = document.createElement('thead');
+    tableEl.appendChild(tableHead);
+    // create the header row
+    var headRow = document.createElement('tr');
+    tableHead.appendChild(headRow);
+    
+    // create header cells
+    headData.forEach(function(headName) {
+        var headCell = document.createElement('th');
+        // create abbreviation tag and set inner text
+        var abbrEl = document.createElement('abbr');
+        abbrEl.innerText = headName;
+        headCell.appendChild(abbrEl);
+        // append cells to the head row
+        headRow.appendChild(headCell);
+    });
+    
+    // create table body and table row data for each team and append to table
+    createTableData(tableEl);
+}
+
+function createTableData(tableEl) {
+    // create table body and append to table
+    var tableBody = document.createElement('tbody');
+    
+}
+
+
 /*------API FUNCTIONS------*/
+
+function createLeague(leagueName) {
+    var manual = league.manualLeague;
+    var sorted = league.sortedLeague;
+    
+    // find table container id and append table element and table caption
+    var container = document.querySelector('#leagueTable');
+    
+    var leagueTable = document.createElement('table');
+    var leagueCaption = document.createElement('caption');
+    // set caption text
+    leagueCaption.innerText = leagueName;
+    
+    container.appendChild(leagueTable);
+    leagueTable.appendChild(leagueCaption)
+    
+    // create the tablehead
+    createTableHead(leagueTable);
+}
 
 
 function addTeam(team, position) {
     // logic for manual positioning
-    if (arguments > 1) {
-        // manual position logic to go here...
+    if (arguments.length > 1) {
+        // team to be positioned manually
+        positionManually(team, position);
+        return;
     }
         
     var sorted = league.sortedLeague;
@@ -110,12 +180,18 @@ function addTeam(team, position) {
     console.log(league);
 }
 
+
 function sort() {
     var sorted = league.sortedLeague;
     var temp = league.tempLeague;
     var zeroTeams = temp.filter(function(team) {
         return !team.points;
-    })
+    });
+    
+    // check for teams with postiion set manually
+    if (league.manualLeague.length) {
+        throw new Error('Sort function can only be called when teams have not had their positions manually set.');
+    }
     
     if (zeroTeams.length) {
         sortWithZeroPoints(zeroTeams);
@@ -158,6 +234,7 @@ function sort() {
     }
 }
 
+
 function updateTeam(name, data) {
     // find team and update with relevant data
     var teamToUpdate = league.sortedLeague.filter(function(sortedTeam) {
@@ -177,6 +254,7 @@ function updateTeam(name, data) {
     
     console.log(league.sortedLeague);
 }
+
 
 function deleteTeam(name) {
     var deleteIndex;
