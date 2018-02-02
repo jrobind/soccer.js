33e6a4c04ec89422f83c553fa2a92c0cd5783a9a
James Robinson <jrobind@outlook.com>
2018-02-02 15:44:07 +0000

    update to team auto re-render and sort - plus auto render of footer update time
    when updateTeam() is invoked any updates will re-render the table and re-sort according to update. User now has option to add footer, if so, the last update time is added automatically to the footer

--- src/scripts/soccer.js
+++ src/scripts/soccer.js
@@ -16,6 +16,69 @@ var cache = {
 /*-------------HELPER FUNCTIONS------------*/
 
 
+function sort(internalSort) {
+    var sorted = league.sortedLeague;
+    var temp = league.tempLeague;
+    var zeroTeams = temp.filter(function(team) {
+        return !team.points;
+    });
+    
+    // check for teams with postiion set manually
+    if (manualPositionCheck()) {
+        throw new Error('Teams that have been positioned manually cannot be sorted.')   
+    }
+    
+    // if sort() is called internally we need to update our arrays
+    if (internalSort) {
+        // push all teams from sorted to temp array and empty the sorted array
+        sorted.forEach(function(team) {
+            temp.push(team)
+        });
+        sorted.length = 0;
+    }
+    
+    // teams with 0 points represents an edge case so we check here
+    if (zeroTeams.length) {
+        sortWithZeroPoints(zeroTeams);
+        return;
+    }
+    
+    // base case
+    if (temp.length === 0) {
+        return;
+    // recursive case
+    } else {
+        for (var i = 0; i < temp.length; i ++) {
+            if (temp[i].points > cache.points) {
+                // update the cache with team data
+                updateCache(temp[i]);  
+            } else if (temp[i].points === cache.points) {
+                // check goal difference
+                goalDifferenceCheck(temp[i]);
+            }
+        }
+        
+        // match the right team(s) and push to sorted league
+        temp.forEach(function(team, index) {
+            if (team.name === cache.team.name) {
+                sorted.push(team);
+                // remove team 
+                temp.splice(index, 1);
+            }
+        });
+        
+        // reset cache
+        cacheReset();
+
+        // recurse until all teams are checked
+        sort();
+        
+        // add positions to teams
+        addPositions(league.sortedLeague);
+    }
+}
+
+
 function updateCache(team) {
     cache.team = team;
     cache.points = team.points;
@@ -59,6 +122,18 @@ function cacheReset() {
 }
 
 
+function lastupdated() {
+    var date = new Date();
+    // format the date
+    var dString = date.toGMTString();
+    // remove 'GMT' from end of date
+    var sliceGmt = dString.slice(0, 25);
+    
+    // set new date onto the league object
+    return 'Last updated ' + sliceGmt;
+}
+
+
 function addPositions(sorted) {
     // add league position to each team object
     sorted.forEach(function(team, index) {
@@ -73,6 +148,18 @@ function manualPositionCheck() {
         return true;
     } else {
         return false;
+    }
+}
+
+
+function internalSortAndRender() {
+    // re-sort the table
+    if (league.positionedManually) {
+        createLeague(league.leagueName, league.colLength);    
+    } else {
+        sort(true);
+        // re-render the table to show sorted updates
+        createLeague(league.leagueName, league.colLength);
     }
 }
 
@@ -118,6 +205,8 @@ function positionManually(team, position) {
     // add to tempArray but sort by manual position given
     league.sortedLeague[arrPos] = team;
     league.positionedManually = true;
+    // re-render the table
+    internalSortAndRender();
 }
 
 
@@ -207,10 +296,8 @@ function createTableFooter(leagueTable) {
     // create span for footer row cell
     var footerTime = document.createElement('time');
     
-    // add most recent last updated time if we have it
-    if (league.lastUpdated) {
-        footerTime.innerText = league.lastUpdated;
-    }
+    // add most recent update time
+        footerTime.innerText = lastupdated();
     // append span to footer cell
     footerCell.appendChild(footerTime);
 }
@@ -219,10 +306,12 @@ function createTableFooter(leagueTable) {
 /*------------API FUNCTIONS------------*/
 
 
-function createLeague(leagueName, colLength) {
+function createLeague(data) {
     var container = document.querySelector('#leagueTable');
     var leagueTable = document.createElement('table');
     var leagueCaption = document.createElement('caption');
+    // first sort the teams
+    sort();
     
     // remove old league if there is one
     var containerChildNodes = [].slice.apply(container.childNodes);
@@ -234,112 +323,43 @@ function createLeague(leagueName, colLength) {
     });
     
     // store league name and column length
-    league['leagueName'] = leagueName;
-    league['colLength'] = colLength;
+    league['leagueName'] = data.leagueName;
+    league['colLength'] = data.colTitle;
     
     // set caption text
-    leagueCaption.innerText = leagueName;
+    leagueCaption.innerText = data.leagueName;
     // append table element and table caption to container div
     container.appendChild(leagueTable);
     leagueTable.appendChild(leagueCaption)
     
     // create the tablehead
-    createTableHead(leagueTable, colLength);
-    // create the tableFooter
-    createTableFooter(leagueTable);
+    createTableHead(leagueTable, data.colTitle);
+    
+    // check if we need to create the tableFooter
+    if (data.footer) {
+        createTableFooter(leagueTable);   
+    }
 }
 
 
 function addTeam(team, position) {
+    var sorted = league.sortedLeague;
+    var temp = league.tempLeague;
+    
     // logic for manual positioning
     if (arguments.length > 1) {
         // team to be positioned manually
         positionManually(team, position);
         return;
     }
-        
-    var sorted = league.sortedLeague;
-    var temp = league.tempLeague;
     
     temp.push(team);
+    console.log(league.tempLeague);
+    
     // check whether we are adding teams to an existing league
     if (sorted.length) {
         league.tempLeague = league.tempLeague.concat(sorted);
         league.sortedLeague.length = 0;
-    }
-}
-
-
-function addLastupdated(date) {
-    // format the date
-    var dString = date.toGMTString();
-    // remove 'GMT' from end of date
-    var sliceGmt = dString.slice(0, 25);
-    
-    // set new date onto the league object
-    league['lastUpdated'] = 'Last updated ' + sliceGmt;
-}
-
-
-function sort(internalSort) {
-    var sorted = league.sortedLeague;
-    var temp = league.tempLeague;
-    var zeroTeams = temp.filter(function(team) {
-        return !team.points;
-    });
-    
-    // check for teams with postiion set manually
-    if (manualPositionCheck()) {
-        throw new Error('Teams that have been positioned manually cannot be sorted.')   
-    }
-    
-    // if sort() is called internally we need to update our arrays
-    if (internalSort) {
-        // push all teams from sorted to temp array and empty the sorted array
-        sorted.forEach(function(team) {
-            temp.push(team)
-        });
-        sorted.length = 0;
-    }
-    
-    // teams with 0 points represents an edge case so we check here
-    if (zeroTeams.length) {
-        sortWithZeroPoints(zeroTeams);
-        return;
-    }
-    
-    // base case
-    if (temp.length === 0) {
-        return;
-    // recursive case
-    } else {
-        for (var i = 0; i < temp.length; i ++) {
-            if (temp[i].points > cache.points) {
-                // update the cache with team data
-                updateCache(temp[i]);  
-            } else if (temp[i].points === cache.points) {
-                // check goal difference
-                goalDifferenceCheck(temp[i]);
-            }
-        }
-        
-        // match the right team(s) and push to sorted league
-        temp.forEach(function(team, index) {
-            if (team.name === cache.team.name) {
-                sorted.push(team);
-                // remove team 
-                temp.splice(index, 1);
-            }
-        });
-        
-        // reset cache
-        cacheReset();
-
-        // recurse until all teams are checked
-        sort();
-        
-        // add positions to teams
-        addPositions(league.sortedLeague);
     }
 }
 
@@ -390,19 +410,8 @@ function updateTeam(name, data) {
             }
         });
     });
-    
-    
-//    internalSortCheck(); !!!!!!!
-    
-    // re-sort the table
-    if (league.positionedManually) {
-        // re-render the table to show updates
-        createLeague(league.leagueName, league.colLength);    
-    } else {
-        sort(true);
-        createLeague(league.leagueName, league.colLength);
-    }
-
+    // re-render the table and sort if we can
+    internalSortAndRender();
 }
 
 
