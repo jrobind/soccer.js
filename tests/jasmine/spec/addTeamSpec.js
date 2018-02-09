dcce1131e66a338b93072c9d43f5a726ab108a8c
James Robinson <jrobind@outlook.com>
2018-02-09 14:32:39 +0000

    add new tests

--- tests/jasmine/spec/addTeamSpec.js
+++ tests/jasmine/spec/addTeamSpec.js
@@ -1,17 +1,28 @@
 describe('addTeam()', function() {
     var team = [{name: 'Merry Argyle', gp: 15, w: 1, d: 0, l: 4, gs: 5, a: 4, gd: -6, pts: 11}];
+    var multipleTeams = [{name: 'Merry Argyle', gp: 15, w: 1, d: 0, l: 4, gs: 5, a: 4, gd: -6, pts: 11}, {name: 'Frodo City', gp: 10, w: 3, d: 2, l: 5, gs: 15, a: 14, gd: -1, pts: 13}];
     var missingProps = [{name: 'Merry Argyle', gp: 15, w: 1}];
     var incorrectprops = [{name: 'Merry Argyle', wrong: 15, w: 1, d: 0, l: 4, gs: 5, a: 4, gd: -6, pts: 11}];
+    var noArray = {name: 'Merry Argyle', gp: 15, w: 1, d: 0, l: 4, gs: 5, a: 4, gd: -6, pts: 11};
     
     beforeEach(function() {
         soccer.league.length = 0;
     });
     
     
-    it('should store a team object', function() {
+    it('should add a team object', function() {
         soccer.addTeam(team);
         
+        expect(soccer.league.length).toBe(1);
         expect(soccer.league[0]).toEqual(jasmine.any(Object));
+    });
+    
+    it('should add multiple team objects', function() {
+        soccer.addTeam(multipleTeams);
+        
+        expect(soccer.league.length).toBe(2);
+        expect(soccer.league[0]).toEqual(jasmine.any(Object));
+        expect(soccer.league[1]).toEqual(jasmine.any(Object));
     });
     
     it('should return league array containing team object', function() {
@@ -55,6 +66,12 @@ describe('addTeam()', function() {
             soccer.addTeam(team);
             expect(soccer.renderLeague).not.toHaveBeenCalled();
         });
+    });
+    
+    it('should throw Error if team object(s) not passed within an array', function() {
+        expect(function(){
+            soccer.addTeam(noArray);
+        }).toThrow(new Error('Invalid argument. Team objects must be passed within an array.'));  
     });
     
     it('should throw Error if incorrect number of properties used', function() {
